28c3087c2b533d7e733dd15335f14ec196d5986c
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-23 01:19:56 +0800

    Create gembel.sh

---
+++ gembel.sh
@@ -0,0 +1,123 @@
+#Author : faizul
+#Tools : Bokep
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+red='\e[1;31m'
+green='\e[1;32m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
+magenta='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+#gantikodewarna
+k="\033[33;1m"
+m="\033[31;1m"
+pu="\033[39;1m"
+p="\033[36;1m"
+b="\033[34;1m"
+h="\033[32;1m"
+JAM=`date +%H`
+MENIT=`date +%M`
+DETIK=`date +%S`
+TANGGAL=`date +%d`
+BULAN=`date +%m`
+TAHUN=`date +%Y`
+mp="\033[41;1;39m"
+o="\033[0m"
+clear
+echo
+echo "         ${m}[ ${pu}TIME ZONE INDONESIA ${m}]"
+echo "${b}   ╔════════╗${b} ╔═══════🕜═╗${b} ╔═══════🕛═╗"
+echo "${b}   ║${k}JAM:"${m}[${pu}$JAM${m}]${b}║~${b}║${k}MENIT:"${m}[${pu}$MENIT${m}]${b}║${b}~║${k}DETIK:${m}["${pu}$DETIK${m}]${b}║
+echo "${b}   ╚════════╝${b} ╚══════════╝${b} ╚══════════╝"
+echo "               ${m}[ ${pu}KALENDER ${m}]"
+echo "${b}╔════════════╗${b} ╔══════════╗${b} ╔════════════╗"
+echo "${b}║${k}TANGGAL:"${m}[${pu}$TANGGAL${m}]${b}║${b}~║${k}BULAN:"${m}[${pu}$BULAN${m}]${b}║${b}~║${k}TAHUN:${m}["${pu}$TAHUN${m}]${b}║
+echo "${b}╚════════════╝${b} ╚══════════╝${b} ╚════════════╝"
+echo "${k}<════════════════════════════════════════>"
+echo
+
+
+limit=100
+sleep 1
+echo " \033[31;1m    ▇▇▇▇▇▇▇"
+echo "\033[37;1m     ▇▇▇▇▇▇▇"
+echo "\033[31;1mSELAMAT DATANG \033[37;1mDi Tools Faizul Kontol lu"
+sleep 1
+echo -n "My Name Is?:"
+read nama;
+sleep 3
+echo "Hallo salam kenal Sad \033[31;1m[$nama]"
+sleep 3
+echo "\033[32;1mJangan Lupa subscribe channel admin woke:)"
+sleep 3
+termux-open-url "https://www.youtube.com/channel/UCSxkYGsMV5DvXz4GQJskl9Q"
+clear
+sleep 3
+cler
+echo $me "
+          ██
+        ▄▀░█          ●DATE : JUM/23/OKTOBER
+████▄▄▄▀░░▀▀▀▀▄  |--------------------------|
+████░░░░░░░░░░█  |   Author : Faizul 
+████░░░░░░░░░░█  |   WHATSA : 082271426251
+████▄▄▄░░░░░░░█  |--------------------------|"
+▀▀▀▀░░░▀▀▀▀▀▀▀
+
+echo $ij "
+[--------------------------------------]
+    [ ●MEMBUKA SITUS WARGA +62 ● ]
+[--------------------------------------]
+[1] ●SITUS SALDA +18 VIRAL SOSMED
+[--------------------------------------]
+[2] ●INSTALL BAHAN NOT EROR
+[--------------------------------------]
+┗────[99] EXIT/TOOLS"
+echo '\033[31;1m'
+read -p "root@Pilih Nomor > " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+sleep 5
+echo $me "✔MEMBUKA SITUS SALDA"
+sleep 5
+echo $ij "DONE"
+termux-open-url "https://salda.ws/video.php?q=Bokep+indo+viral+2020"
+cd Warga62
+sh gembel.sh
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+sleep 5
+echo $me "✔INSTALL BAHAN SABAR"
+clear
+pkg install upgrade && pkg install update
+pkg install curl
+pkg instalk python2
+pip2 install gem
+pip2 install mechanize
+pip2 install bs4
+pkg install git
+pip2 install requests
+pkg install ruby
+pkg install nano
+pkg install php
+pkg install tq
+echo $ij "install Bahan Done"
+cd Warga62
+sh gembel.sh
+fi
+
+if [ $bro = 99 ] || [ $bro = 99 ]
+then
+echo $cyan  "Subscribe Channel Htc Ctr Gaming "
+sleep 1
+exit
+fi
